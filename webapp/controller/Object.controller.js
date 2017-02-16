0dff4a4b36ff263b2263da9981d09e79738a6d10
Liang Liang Wang <liang.liang.wang@sap.com>
2017-02-16 07:40:50 +0000

    Adding a Comments Section Extend the product detail view by adding a feature allowing to add comments to the product

--- webapp/controller/Object.controller.js
+++ webapp/controller/Object.controller.js
@@ -1,167 +1,197 @@
 /*global location*/
 sap.ui.define([
-		"mycompany/myapp/controller/BaseController",
-		"sap/ui/model/json/JSONModel",
-		"sap/ui/core/routing/History",
-		"mycompany/myapp/model/formatter"
-	], function (
-		BaseController,
-		JSONModel,
-		History,
-		formatter
-	) {
-		"use strict";
+	"mycompany/myapp/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/routing/History",
+	"mycompany/myapp/model/formatter",
+	"sap/ui/core/format/DateFormat",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(
+	BaseController,
+	JSONModel,
+	History,
+	formatter, DateFormat, Filter, FilterOperator
+) {
+	"use strict";
 
-		return BaseController.extend("mycompany.myapp.controller.Object", {
+	return BaseController.extend("mycompany.myapp.controller.Object", {
 
-			formatter: formatter,
+		formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
 
-			/**
-			 * Called when the worklist controller is instantiated.
-			 * @public
-			 */
-			onInit : function () {
-				// Model used to manipulate control states. The chosen values make sure,
-				// detail page is busy indication immediately so there is no break in
-				// between the busy indication for loading the view's meta data
-				var iOriginalBusyDelay,
-					oViewModel = new JSONModel({
-						busy : true,
-						delay : 0
-					});
+		/**
+		 * Called when the worklist controller is instantiated.
+		 * @public
+		 */
+		onInit: function() {
+			// Model used to manipulate control states. The chosen values make sure,
+			// detail page is busy indication immediately so there is no break in
+			// between the busy indication for loading the view's meta data
+			var iOriginalBusyDelay,
+				oViewModel = new JSONModel({
+					busy: true,
+					delay: 0
+				});
 
-				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+			this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
 
-				// Store original busy indicator delay, so it can be restored later on
-				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
-				this.setModel(oViewModel, "objectView");
-				this.getOwnerComponent().getModel().metadataLoaded().then(function () {
-						// Restore original busy indicator delay for the object view
-						oViewModel.setProperty("/delay", iOriginalBusyDelay);
-					}
-				);
-			},
+			// Store original busy indicator delay, so it can be restored later on
+			iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+			this.setModel(oViewModel, "objectView");
+			this.getOwnerComponent().getModel().metadataLoaded().then(function() {
+				// Restore original busy indicator delay for the object view
+				oViewModel.setProperty("/delay", iOriginalBusyDelay);
+			});
+		},
 
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
 
-			/**
-			 * Event handler when the share in JAM button has been clicked
-			 * @public
-			 */
-			onShareInJamPress : function () {
-				var oViewModel = this.getModel("objectView"),
-					oShareDialog = sap.ui.getCore().createComponent({
-						name: "sap.collaboration.components.fiori.sharing.dialog",
-						settings: {
-							object:{
-								id: location.href,
-								share: oViewModel.getProperty("/shareOnJamTitle")
-							}
-						}
-					});
-				oShareDialog.open();
-			},
-
-			/**
-			 * Event handler  for navigating back.
-			 * It there is a history entry or an previous app-to-app navigation we go one step back in the browser history
-			 * If not, it will replace the current entry of the browser history with the worklist route.
-			 * @public
-			 */
-			onNavBack : function() {
-				var sPreviousHash = History.getInstance().getPreviousHash(),
-					oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
-
-				if (sPreviousHash !== undefined || !oCrossAppNavigator.isInitialNavigation()) {
-					history.go(-1);
-				} else {
-					this.getRouter().navTo("worklist", {}, true);
-				}
-			},
-
-			/* =========================================================== */
-			/* internal methods                                            */
-			/* =========================================================== */
-
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-			 * @private
-			 */
-			_onObjectMatched : function (oEvent) {
-				var sObjectId =  oEvent.getParameter("arguments").objectId;
-				this.getModel().metadataLoaded().then( function() {
-					var sObjectPath = this.getModel().createKey("Products", {
-						ProductID :  sObjectId
-					});
-					this._bindView("/" + sObjectPath);
-				}.bind(this));
-			},
-
-			/**
-			 * Binds the view to the object path.
-			 * @function
-			 * @param {string} sObjectPath path to the object to be bound
-			 * @private
-			 */
-			_bindView : function (sObjectPath) {
-				var oViewModel = this.getModel("objectView"),
-					oDataModel = this.getModel();
-
-				this.getView().bindElement({
-					path: sObjectPath,
-					events: {
-						change: this._onBindingChange.bind(this),
-						dataRequested: function () {
-							oDataModel.metadataLoaded().then(function () {
-								// Busy indicator on view should only be set if metadata is loaded,
-								// otherwise there may be two busy indications next to each other on the
-								// screen. This happens because route matched handler already calls '_bindView'
-								// while metadata is loaded.
-								oViewModel.setProperty("/busy", true);
-							});
-						},
-						dataReceived: function () {
-							oViewModel.setProperty("/busy", false);
+		/**
+		 * Event handler when the share in JAM button has been clicked
+		 * @public
+		 */
+		onShareInJamPress: function() {
+			var oViewModel = this.getModel("objectView"),
+				oShareDialog = sap.ui.getCore().createComponent({
+					name: "sap.collaboration.components.fiori.sharing.dialog",
+					settings: {
+						object: {
+							id: location.href,
+							share: oViewModel.getProperty("/shareOnJamTitle")
 						}
 					}
 				});
-			},
+			oShareDialog.open();
+		},
 
-			_onBindingChange : function () {
-				var oView = this.getView(),
-					oViewModel = this.getModel("objectView"),
-					oElementBinding = oView.getElementBinding();
+		/**
+		 * Event handler  for navigating back.
+		 * It there is a history entry or an previous app-to-app navigation we go one step back in the browser history
+		 * If not, it will replace the current entry of the browser history with the worklist route.
+		 * @public
+		 */
+		onNavBack: function() {
+			var sPreviousHash = History.getInstance().getPreviousHash(),
+				oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
 
-				// No data for the binding
-				if (!oElementBinding.getBoundContext()) {
-					this.getRouter().getTargets().display("objectNotFound");
-					return;
+			if (sPreviousHash !== undefined || !oCrossAppNavigator.isInitialNavigation()) {
+				history.go(-1);
+			} else {
+				this.getRouter().navTo("worklist", {}, true);
+			}
+		},
+
+		/* =========================================================== */
+		/* internal methods                                            */
+		/* =========================================================== */
+
+		/**
+		 * Binds the view to the object path.
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+		 * @private
+		 */
+		_onObjectMatched: function(oEvent) {
+			var sObjectId = oEvent.getParameter("arguments").objectId;
+			this.getModel().metadataLoaded().then(function() {
+				var sObjectPath = this.getModel().createKey("Products", {
+					ProductID: sObjectId
+				});
+				this._bindView("/" + sObjectPath);
+			}.bind(this));
+		},
+
+		/**
+		 * Binds the view to the object path.
+		 * @function
+		 * @param {string} sObjectPath path to the object to be bound
+		 * @private
+		 */
+		_bindView: function(sObjectPath) {
+			var oViewModel = this.getModel("objectView"),
+				oDataModel = this.getModel();
+
+			this.getView().bindElement({
+				path: sObjectPath,
+				events: {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function() {
+						oDataModel.metadataLoaded().then(function() {
+							// Busy indicator on view should only be set if metadata is loaded,
+							// otherwise there may be two busy indications next to each other on the
+							// screen. This happens because route matched handler already calls '_bindView'
+							// while metadata is loaded.
+							oViewModel.setProperty("/busy", true);
+						});
+					},
+					dataReceived: function() {
+						oViewModel.setProperty("/busy", false);
+					}
 				}
+			});
+		},
 
-				var oResourceBundle = this.getResourceBundle(),
-					oObject = oView.getBindingContext().getObject(),
-					sObjectId = oObject.ProductID,
-					sObjectName = oObject.ProductName;
+		_onBindingChange: function() {
+			var oView = this.getView(),
+				oViewModel = this.getModel("objectView"),
+				oElementBinding = oView.getElementBinding();
 
-				// Everything went fine.
-				oViewModel.setProperty("/busy", false);
-				oViewModel.setProperty("/saveAsTileTitle", oResourceBundle.getText("saveAsTileTitle", [sObjectName]));
-				oViewModel.setProperty("/shareOnJamTitle", sObjectName);
-				oViewModel.setProperty("/shareSendEmailSubject",
-				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-				oViewModel.setProperty("/shareSendEmailMessage",
-				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+			// No data for the binding
+			if (!oElementBinding.getBoundContext()) {
+				this.getRouter().getTargets().display("objectNotFound");
+				return;
 			}
 
-		});
+			var oResourceBundle = this.getResourceBundle(),
+				oObject = oView.getBindingContext().getObject(),
+				sObjectId = oObject.ProductID,
+				sObjectName = oObject.ProductName;
 
-	}
-);
+			// Everything went fine.
+			oViewModel.setProperty("/busy", false);
+			oViewModel.setProperty("/saveAsTileTitle", oResourceBundle.getText("saveAsTileTitle", [sObjectName]));
+			oViewModel.setProperty("/shareOnJamTitle", sObjectName);
+			oViewModel.setProperty("/shareSendEmailSubject",
+				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+			oViewModel.setProperty("/shareSendEmailMessage",
+				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+
+			// Update the comments in the list
+			var oList = this.byId("idCommentsList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(new Filter("productID", FilterOperator.EQ, sObjectId));
+		},
+		/**
+      * Updates the model with the user comments on Products.
+      * @function
+      * @param {sap.ui.base.Event} oEvent object of the user input
+      */
+      onPost: function (oEvent) {
+         var oFormat = DateFormat.getDateTimeInstance({style: "medium"});
+         var sDate = oFormat.format(new Date());
+         var oObject = this.getView().getBindingContext().getObject();
+         var sValue = oEvent.getParameter("value");
+         var oEntry = {
+             productID: oObject.ProductID,
+             type: "Comment",
+             date: sDate,
+             comment: sValue
+         };        
+         // update model
+         var oFeedbackModel = this.getModel("productFeedback");
+         var aEntries = oFeedbackModel.getData().productComments;
+         aEntries.push(oEntry);
+         oFeedbackModel.setData({
+            productComments : aEntries
+         });
+      }
+
+	});
+
+});
